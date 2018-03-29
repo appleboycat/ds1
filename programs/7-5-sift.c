885a4d167015812b5d866fd647be3c6c6defd4f0
Ella <ella@ella.ella>
2018-03-29 16:04:46 +0800

    [7-5] HeapAdjust + HeapSort

--- programs/7-5-sift.c
+++ programs/7-5-sift.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 typedef int data;
+void HeapAdjust(data* H, int s, int end);
+void HeapSort(data* H, int n);
+
 void ReadHeap(data* H, int n);
-void InitHeap(data* H, int n);
-void sift(data* H, int s, int end);	
 int main()
 {
-	freopen("7-5-data.in", "r", stdin);
-	int n, m;
-	scanf("%d %d\n", &n, &m);
+	freopen("7-5-sift-data.in", "r", stdin);
+	int n;
+	scanf("%d\n", &n);
 	data* H = (data*)malloc(sizeof(data) * (n+1));	
 	ReadHeap(H, n);
 	fclose("stdin");
@@ -28,7 +29,7 @@ void ReadHeap(data* H, int n)
 		printf("%d ", H[i]);
 	}
 	printf("\n");
-	InitHeap(H, n);
+	HeapSort(H, n);
 	printf("after:\n");
 	for(int i = 1; i <= n; i++)
 	{
@@ -36,14 +37,40 @@ void ReadHeap(data* H, int n)
 	}
 	printf("\n");
 }
-void InitHeap(data* H, int n)
+void HeapSort(data* H, int n)
 {
 	for(int i = n/2; i > 0; i--)
 	{
-		sift(H, i, n);	
+		HeapAdjust(H, i, n);	
 	}	
+
+	for(int i = n; i > 1; i--)
+	{
+		data tmp = H[i];
+		H[i] = H[1];
+		H[1] = tmp;
+		HeapAdjust(H, 1, i-1);
+	}
 }
-void sift(data* H, int s, int end)	
+
+void HeapAdjust(data* H, int s, int end)
+{
+	printf("%s\n", __func__);
+	data x = H[s];
+	for(int j = s*2; j <= end; j = s*2)
+	{
+		if((H[j+1] < H[j])&&(j < end))
+			j++;
+		printf("H[%d]=%d  H[%d]=%d\n", s, H[s], j, H[j]);
+		if(x < H[j])
+			break;
+		H[s] = H[j];
+		s = j;
+		printf("switch H[%d]=%d  H[%d]=%d\n", s, H[s], j, H[j]);
+	}
+	H[s] = x;
+}
+/*void sift(data* H, int s, int end)	
 {
 	printf("%s\n", __func__);
 	int i = s;
@@ -66,3 +93,4 @@ void sift(data* H, int s, int end)
 			break;
 	}
 }
+*/
