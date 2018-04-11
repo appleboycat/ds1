6173df779d8985f8bf2eedf3b12713b55aaf4ecd
Ella <ella@ella.ella>
2018-04-11 20:38:20 +0800

    [7-6] Graph DFS/BFS 1.metrix

---
+++ programs/7-6-metrix.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXV 10
+#define MAXQ 10
+
+typedef int vtype;
+typedef struct graph
+{	
+	int		vxnum;
+	int		egnum;
+	int		edge[MAXV][MAXV];
+	vtype	vertex[MAXV];
+}Mgraph;
+	
+void BFS(Mgraph *m, int *visit, int start)
+{
+	if(visit[start] != 0)
+		return;
+	printf(" %d", start);
+	visit[start] = 1;
+	int	que[MAXQ];
+	int front = 0, rear = 0;
+	que[rear] = start;
+	rear = (rear + 1) % MAXQ;
+	while( rear != front)
+	{
+		int tmp = que[front];
+		front = (front + 1) % MAXQ;
+		for(int i = 0; i < m->vxnum; i++)
+		{
+			if((visit[i] == 0) && (m->edge[tmp][i] == 1))
+			{
+				printf(" %d", i);
+				visit[i] = 1;
+				//if(((rear + 1) % MAXQ) != front)
+				//{
+					que[rear] = i;  
+					rear = (rear + 1) % MAXQ;
+				//}
+				//else
+			    //	printf("Error: que is full!!\n");
+			}
+		}			
+	}
+}
+		
+void DFS(Mgraph *m, int *visit, int start)
+{
+	if(visit[start] != 0)
+		return;
+	else
+	{
+		visit[start] = 1;
+		printf(" %d", start);
+		for(int i = 0; i < m->vxnum; i++)
+		{
+			if((m->edge[start][i] == 1)&&(visit[i] == 0))
+			{
+				DFS(m, visit, i);
+				//	break;
+			}
+		}
+	}
+}
+
+int main()
+{
+	freopen("7-6-data.in.tmp", "r", stdin);
+	Mgraph *m = (Mgraph*)malloc(sizeof(Mgraph));
+	scanf("%d %d\n", &(m->vxnum), &(m->egnum));
+	int vn = m->egnum;
+	while(vn --)
+	{
+		int i, j;
+		scanf("%d %d", &i, &j);
+		m->edge[i][j] = 1;
+		m->edge[j][i] = 1;
+	}
+
+	int visit[MAXQ] = {0};
+	for(int i = 0; i < m->vxnum; i++)
+	{	
+		if(visit[i] == 0)
+		{
+			printf("{");
+			DFS(m, visit, i);
+			printf(" }\n");
+		}
+	}
+	
+//	memset(&visit, 0, sizeof(MAXQ * sizeof(int)));
+//	memset(visit, 0, sizeof(visit));
+//	printf("intsize %d, size: %d\n", sizeof(int), sizeof(MAXQ * sizeof(int)));
+	for(int i = 0; i < MAXV; i++)
+		visit[i] = 0;
+		//printf("%d ", visit[i]);
+	for(int i = 0; i < m->vxnum; i++)
+	{
+		if(visit[i] == 0)
+		{
+			printf("{");
+			BFS(m, visit, i);
+			printf(" }\n");
+		}
+	}
+
+	fclose(stdin);
+	return 0;
+}
